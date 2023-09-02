9b1d96a1272a98f3bac0f0b2301f451762038433
Sameer Alam <alamsameer738@gmail.com>
2023-09-02 23:23:03 +0530

    added time interval categores

--- src/controllers/statisticContoller.js
+++ src/controllers/statisticContoller.js
@@ -102,7 +102,7 @@ export const getTopCategories = async (req, res) => {
                 }
             },
             { $sort: { totalQuantity: -1 } },
-            { $limit: 4 },     
+            { $limit: 4 },
         ]);
 
         res.status(200).json(topCategories);
@@ -111,8 +111,77 @@ export const getTopCategories = async (req, res) => {
     }
 };
 // Get stats for weekly status:
-export const getWeeklyStatus = async (req, res) => {
+export const getcategoriesWithinInterval = async (req, res) => {
+    try { 
+        // Example usage:
+        const{startDate,endDate}=req.body
+        console.log("got request tiem interval",{startDate,endDate});
+        // console.log({startDate,endDate})
+        // const startDate = '2023-8-26'; // Replace with your start date
+        // const endDate = '2023-8-30';   // Replace with your end date
+        const categoriesWithinInterval = await ShoppingList.aggregate([
+
+            { $unwind: '$items' },
+            { $unwind: '$items.purchaseHistory' },
+            {
+                $match: {
+                    'items.purchaseHistory.date': {
+                        $gte: new Date(startDate), // Convert startDate to Date object
+                        $lte: new Date(endDate),   // Convert endDate to Date object
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: '$items.item',
+                    totalQuantity: { $sum: '$items.purchaseHistory.count' }
+                }
+            },
+            {
+                $lookup: {
+                    from: 'items', // Name of the Item collection
+                    localField: '_id',
+                    foreignField: '_id',
+                    as: 'itemData'
+                }
+            },
+            {
+                $unwind: '$itemData'
+            },
+            {
+                $lookup: {
+                    from: 'categories', // Name of the Categories collection
+                    localField: 'itemData.category', // Assuming category field in itemData holds the category ID
+                    foreignField: '_id',
+                    as: 'categoryData'
+                }
+            },
+            {
+                $unwind: '$categoryData'
+            },
+            {
+                $project: {
+                    _id: 1,
+                    totalQuantity: 1,
+                    categoryName: '$categoryData.name'
+                }
+            },
+            {
+                $group: {
+                    _id: '$categoryName',
+                    totalQuantity: { $sum: '$totalQuantity' }
+                }
+            },
+        ]);
+        res.status(200).json(categoriesWithinInterval)
+    }
+    catch (e) {
+        console.log(e.message)
+        res.status(500).send({message:"some error has been occured"})
+    }
+
 };
 // Get stats for monthly status:
 export const getMonthlyStatus = async (req, res) => {
+
 } 
--- src/routes/statisticRoute.js
+++ src/routes/statisticRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { getTopItems,getTopCategories } from "../controllers/statisticContoller.js";
+import { getTopItems,getTopCategories,getcategoriesWithinInterval} from "../controllers/statisticContoller.js";
 
 const router = express.Router();
 // Get top items: GET /api/statistics/top-items -> 
 router.get("/top-items", getTopItems);
 // Get top categories: GET /api/statistics/top-categories -> 
 router.get("/top-categories", getTopCategories);
-// Get monthly status: GET /api/statistics/monthly-status -> 
+// Get monthly status: GET /api/statistics/specified-status -> 
+
+router.post("/time-interval",getcategoriesWithinInterval)
+
 
 export default router;
